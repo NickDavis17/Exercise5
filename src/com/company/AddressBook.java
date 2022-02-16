bf722bf4fff7d20cbcf4d634ff2650c6c4ddf11a
NickDavis17 <063815@swcsd.us>
2022-02-16 13:54:59 -0500

    Final

--- src/com/company/AddressBook.java
+++ src/com/company/AddressBook.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.sql.SQLOutput;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -10,32 +11,62 @@ class AddressBook {
     public static void main(String[] args) {
 
 
-        HashMap<Integer, Person> map = new HashMap<>(){
+        HashMap<String, String> map = new HashMap<>() {
             @Override
             public String toString() {
                 StringBuilder stb = new StringBuilder();
-                for (HashMap.Entry<Integer, Person> entry : this.entrySet()) {
+                for (HashMap.Entry<String, String> entry : this.entrySet()) {
                     stb.append(entry.getKey()).append(" ")
                             .append(entry.getValue()).append("\n");
                 }
                 return stb.toString();
             }
         };
+        System.out.println("Please Select and option:");
+        System.out.println("1: Add person to directory");
+        System.out.println("2: Search for a person's email");
+        System.out.println("3: End");
+        int choice = input.nextInt();
+        //input.nextLine();
+        while (choice != 3) {
+            if (choice == 1) {
+                collectingInputs(map);
 
-        collectingInputs(map);
-        
+            }
+
+            if (choice == 2) {
+                display(map);
+                input.nextLine();
+                System.out.println("Please enter a name");
+                String searchName = input.nextLine();
+                if (map.containsKey(searchName)) {
+                    System.out.println(map.get(searchName));
+                } else {
+                    System.out.println("Sorry, that person does not exist");
+                }
+            }
+            System.out.println("Please Select and option:");
+            System.out.println("1: Add person to directory");
+            System.out.println("2: Search for a person's email");
+            System.out.println("3: End");
+
+            choice = input.nextInt();
+
+        }
         display(map);
 
     }
-    static void collectingInputs(HashMap<Integer, Person> map){
+
+    static HashMap<String, String> collectingInputs(HashMap<String, String> map) {
         int num = 1;
         System.out.println("What is the name of your person");
+        input.nextLine();
         String name = input.nextLine();
         System.out.println("What is their email address?");
         String email = input.nextLine();
         Person aPerson = new Person(name, email);
-        map.put(num,aPerson);
-        while(!(name.equalsIgnoreCase("End"))){
+        map.put(name, email);
+        /*while(!(name.equalsIgnoreCase("End"))){
 
             System.out.println("What name does your person have");
             name = input.nextLine();
@@ -46,14 +77,14 @@ class AddressBook {
             System.out.println("What is their email address?");
             email = input.nextLine();
 
-            Person newPerson = new Person(name, email);
-            num += 1;
-            map.put(num, newPerson);
-
-        }
-
+            Person newPerson = new Person(name, email);*/
+        num += 1;
+        map.put(name, email);
+        System.out.println("Your person has been created. " + aPerson);
+        return map;
     }
-    static void display(HashMap<Integer, Person> map){
+
+    static void display(HashMap<String, String> map) {
         System.out.println(map);
     }
 }
